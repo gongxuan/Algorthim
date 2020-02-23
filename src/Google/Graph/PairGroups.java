001b313fced06d06c92c11b5743886f05fa2e867
xuan gong <xuangong@xuans-MacBook-Pro.local>
2020-02-23 15:23:51 -0800

    set up env on macbook2019

--- src/Google/Graph/PairGroups.java
+++ src/Google/Graph/PairGroups.java
@@ -1,31 +1,38 @@
 package Google.Graph;
 
-import com.sun.tools.javac.util.Pair;
 import java.util.*;
 
 
 //https://www.1point3acres.com/bbs/forum.php?mod=viewthread&tid=573578&extra=page%3D1%26filter%3Dsortid%26sortid%3D311%26searchoption%5B3088%5D%5Bvalue%5D%3D1%26searchoption%5B3088%5D%5Btype%5D%3Dradio%26searchoption%5B3089%5D%5Bvalue%5D%5B3%5D%3D3%26searchoption%5B3089%5D%5Btype%5D%3Dcheckbox%26searchoption%5B3046%5D%5Bvalue%5D%3D1%26searchoption%5B3046%5D%5Btype%5D%3Dradio%26sortid%3D311%26orderby%3Ddateline
 //第一轮：给一个vector<pair<string, string>>，一个pair中的两个string属于同一组，问一共有几组。答得不好。
+class Pair{
+    String first,second;
+    public Pair(String f,String s){
+        this.first = f;
+        this.second  = s;
+    }
+}
 public class PairGroups {
+
   //clarify: list length, string len? repeating pairs?
   //convert question to number of unconnected sub-graphs
   public static void main(String[] args) {
     PairGroups pairGroups = new PairGroups();
-    List<Pair<String,String>> data = new ArrayList<>();
-    data.add(new Pair<>("A","B"));
-    data.add(new Pair<>("A","B"));
-    data.add(new Pair<>("A","C"));
-    data.add(new Pair<>("D","E"));
+    List<Pair> data = new ArrayList<>();
+    data.add(new Pair("A","B"));
+    data.add(new Pair("A","B"));
+    data.add(new Pair("A","C"));
+    data.add(new Pair("D","E"));
     System.out.println(pairGroups.countByUF(data) +" "+ pairGroups.countDFS(data));
 
   }
-  public int countDFS(List<Pair<String,String>> data){
+  public int countDFS(List<Pair> data){
     //construct graph
     //dfs+ visited Time:O(V+E+N), N is list length
     Map<String,Set<String>> graph = new HashMap<>();
-    for(Pair<String,String> pair:data){
-      String str1 = pair.fst;
-      String str2=  pair.snd;
+    for(Pair pair:data){
+      String str1 = pair.first;
+      String str2=  pair.second;
       graph.computeIfAbsent(str1,v->new HashSet<>()).add(str2);
       graph.computeIfAbsent(str2,v->new HashSet<>()).add(str1);
     }
@@ -54,11 +61,11 @@ public class PairGroups {
     }
   }
 
-  public int countByUF(List<Pair<String,String>> data){
+  public int countByUF(List<Pair> data){
     Set<String> set= new HashSet<>();
-    for(Pair<String,String> pair:data){
-      set.add(pair.fst);
-      set.add(pair.snd);
+    for(Pair pair:data){
+      set.add(pair.first);
+      set.add(pair.second);
     }
     //list of unique Strings
     Map<String,Integer> strIdx = new HashMap<>();
@@ -67,9 +74,9 @@ public class PairGroups {
       strIdx.put(str,idx++);
     }
     UF uf = new UF(strIdx.size());
-    for(Pair<String,String> pair:data){
-      int idx1 = strIdx.get(pair.fst);
-      int idx2 = strIdx.get(pair.snd);
+    for(Pair pair:data){
+      int idx1 = strIdx.get(pair.first);
+      int idx2 = strIdx.get(pair.second);
       uf.union(idx1,idx2);
     }
     return uf.groups;
